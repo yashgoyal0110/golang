bee2ceded064b3646c6a90d668023897c33320b7
yashgoyal0110 <yg364550@gmail.com>
2025-09-18 12:39:20 +0530

    go_basics

---
+++ 1_golang/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	// for i := range 10 {
+	// 	fmt.Println(i)
+	// }
+	a := 
+	1
+	b := 1
+	fmt.Println(a == b)
+	// for {
+	// 	fmt.Println("Infinite loop")
+	// }
+}
+
+// main()
