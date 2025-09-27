03af7a2afb5a47b8d448b01914e1c9e6e79ca819
yashgoyal0110 <yg364550@gmail.com>
2025-09-28 01:50:58 +0530

    new-snippets

--- 1_golang/main.go
+++ 1_golang/main.go
@@ -3,16 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
-	for i := range 10 {
-		fmt.Println(i)
-	}
-	a := 
-	1
-	b := 1
-	fmt.Println(a == b)
-	for {
-		fmt.Println("Infinite loop")
-	}
+	num := 6
+	fmt.Println(&num)
+	num2 :=  7
+	fmt.Println(&num2)
 }
-
